28523d20999bda9d53f7e090ebc17e737a7da638
Wasim Adil <waseemadilcode@gmail.com>
2025-08-11 23:14:27 +0500

    Some Changing

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -181,7 +181,7 @@ const Resume = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="absolute -bottom-1 -right-1 w-5 h-5 bg-white rounded-full flex items-center justify-center">
+                  <div className="absolute -bottom-1 -right-1 w-5 h-5 bg-green-500 rounded-full flex items-center justify-center">
                     <div className="w-2.5 h-2.5 bg-green-500 rounded-full"></div>
                   </div>
                 </div>
@@ -282,13 +282,13 @@ const Resume = () => {
                       <div className="w-full h-full bg-gray-800 flex items-center justify-center" style={{display: 'none'}}>
                         <div className="text-center">
                           <div className="w-16 h-16 bg-white/10 rounded-full flex items-center justify-center mx-auto mb-3">
-                            <User size={24} className="text-gray-400" />
+                        
                           </div>
                           {/* <p className="text-gray-500 text-xs font-light">Profile Photo</p> */}
                         </div>
                       </div>
                     </div>
-                    <div className="absolute -bottom-2 -right-2 w-6 h-6 bg-white rounded-full flex items-center justify-center">
+                    <div className="absolute -bottom-2 -right-2 w-6 h-6 bg-green-500 rounded-full flex items-center justify-center">
                       <div className="w-3 h-3 bg-green-500 rounded-full"></div>
                     </div>
                   </div>
